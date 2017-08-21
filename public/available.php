906ac8d972bee41e8c1e428ccd718e8844cdf5cf
Tom Veale <tom@theveales.com.au>
2017-08-22 07:57:30 +1000

    Index updates

--- public/available.php
+++
@@ -1,8 +0,0 @@
-<div class="blockmain <?php echo $blockClass?>">
-    <div class="row  align-center">
-        <div class="column small-12 center-content">
-            <h1><?php echo $message?></h1>
-            <i class="ti <?php echo $icon?>" aria-hidden="true"></i>     
-        </div> 
-    </div>
-<div> 
